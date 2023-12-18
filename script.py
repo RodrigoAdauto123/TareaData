e9f8cbe5b0055e42b639c800054c1d3737b74aae
RodrigoAdauto123 <adauto.rodrigo@pucp.pe>
2023-12-17 21:55:02 -0500

    add .gitignore and .env for connection to db

--- script.py
+++ script.py
@@ -1,24 +1,28 @@
 from sqlalchemy import create_engine
 import requests
 import pandas as pd
+import os
+from dotenv import load_dotenv
 from pandas import json_normalize
 
+# Obteniendo la variables de entorno
+load_dotenv()
+db_name = os.getenv("db_name") # data-engineer-database
+username = os.getenv("username") # adauto_rodrigo_coderhouse
+password = os.getenv("password")
+table_name = os.getenv("table_name") # crypto
 
 url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"
-databaseURI = "postgresql://adauto_rodrigo_coderhouse:l3Fr2953aE@data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com:5439/data-engineer-database"
-table_name = "crypto"
+databaseURI = "postgresql://" + username + ":" + password+"@data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com:5439/" + db_name
 
 conn = create_engine(databaseURI)
 response = requests.get(url)
 
 if response.status_code == 200:
     data = response.json()
-    # df = pd.DataFrame(data)
     df = json_normalize(data)
     df = df.drop_duplicates()
     df.to_sql(table_name, conn, if_exists="replace", index=False)
     conn.dispose()
-
 else:
     print("Error: ", response.status_code)
-
